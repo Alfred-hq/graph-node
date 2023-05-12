37963c8ad1368e6bf12586d449e9fb02c59b9017
Krishnanand V P <vpkrishnanand@gmail.com>
2023-05-12 17:46:14 +0530

    store,graph,node : Add methods to update non_fatal_errors field in db, and wired them in runner

--- store/postgres/src/deployment_store.rs
+++ store/postgres/src/deployment_store.rs
@@ -1631,7 +1631,7 @@ impl DeploymentStore {
                     let _ = self.revert_block_operations(site.clone(), parent_ptr.clone(), &FirehoseCursor::None)?;
 
                     // Unfail the deployment.
-                    deployment::update_deployment_status(conn, deployment_id, prev_health, None)?;
+                    deployment::update_deployment_status(conn, deployment_id, prev_health, None,None)?;
 
                     Ok(UnfailOutcome::Unfailed)
                 }
@@ -1661,6 +1661,26 @@ impl DeploymentStore {
                     Ok(UnfailOutcome::Noop)
                 }
             }
+        })
+    }
+
+    // If a deterministic error happens and nonFatalErrors is set, we should
+    // update the nonFatalErrors field in the deployment table.
+    pub(crate) fn update_non_fatal_errors(
+        &self,
+        site: Arc<Site>,
+        errors: Option<Vec<SubgraphError>>,
+    ) -> Result<(), StoreError> {
+        let conn = &self.get_conn()?;
+        let deployment_id = &site.deployment;
+        conn.transaction(|| {
+            deployment::update_non_fatal_errors(
+                conn,
+                deployment_id,
+                deployment::SubgraphHealth::Unhealthy,
+                errors,
+            )?;
+            Ok(())
         })
     }
 
@@ -1713,6 +1733,7 @@ impl DeploymentStore {
                             conn,
                             deployment_id,
                             deployment::SubgraphHealth::Healthy,
+                            None,
                             None,
                         )?;
 
